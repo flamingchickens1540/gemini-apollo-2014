097b069df6e0ab23489b8ab3b74f1fd1bc06de67
Conner Hansen <hansenc@catlin.edu>
2014-01-15 16:56:28 -0800

    Drive code updated and shifting added.

--- Gemini-Apollo/src/org/team1540/geminiapollo/DriveCode.java
+++ Gemini-Apollo/src/org/team1540/geminiapollo/DriveCode.java
@@ -2,50 +2,66 @@ package org.team1540.geminiapollo;
 
 import ccre.chan.BooleanInputPoll;
 import ccre.chan.BooleanOutput;
+import ccre.chan.BooleanStatus;
 import ccre.chan.FloatFilter;
 import ccre.chan.FloatInputPoll;
 import ccre.chan.FloatOutput;
 import ccre.ctrl.Mixing;
+import ccre.event.EventConsumer;
 import ccre.event.EventSource;
 import ccre.log.Logger;
 
 public class DriveCode {
 
-    public static void createDrive(EventSource begin, EventSource during, FloatOutput leftDrive1, FloatOutput leftDrive2, FloatOutput rightDrive1, FloatOutput rightDrive2, FloatInputPoll leftDriveAxis, FloatInputPoll rightDriveAxis, FloatInputPoll forwardDriveAxis) {
+    /*TO DO:
+    -make sure shifting boolean is correct
+    -add motor adjustors...
+    */
+    public static void createDrive(EventSource begin, EventSource during, final FloatOutput leftDrive1, final FloatOutput leftDrive2, final FloatOutput rightDrive1, final FloatOutput rightDrive2, FloatInputPoll leftDriveAxis, FloatInputPoll rightDriveAxis, FloatInputPoll forwardDriveAxis) {
         Logger.warning("DriveCode TODO");
         //dead zone
         FloatFilter deadZone = Mixing.deadzone(.05f);
-        FloatInputPoll leftDriveAxisW = deadZone.wrap(leftDriveAxis);
-        FloatInputPoll rightDriveAxisW = deadZone.wrap(rightDriveAxis);
-        FloatInputPoll forwardDriveAxisW = deadZone.wrap(forwardDriveAxis);
+        final FloatInputPoll leftDriveAxisW = deadZone.wrap(leftDriveAxis);
+        final FloatInputPoll rightDriveAxisW = deadZone.wrap(rightDriveAxis);
+        final FloatInputPoll forwardDriveAxisW = deadZone.wrap(forwardDriveAxis);
         
-        //motor values
-        float leftDriveValue1;
-        float leftDriveValue2;
-        float rightDriveValue1;
-        float rightDriveValue2;
-        if (forwardDriveAxisW.readValue() != 0) {
-            leftDriveValue1 = forwardDriveAxisW.readValue();
-            leftDriveValue2 = forwardDriveAxisW.readValue();
-            rightDriveValue1 = forwardDriveAxisW.readValue();
-            rightDriveValue2 = forwardDriveAxisW.readValue();
-        } else {
-            leftDriveValue1 = leftDriveAxisW.readValue();
-            leftDriveValue2 = leftDriveAxisW.readValue();
-            rightDriveValue1 = rightDriveAxisW.readValue();
-            rightDriveValue2 = rightDriveAxisW.readValue();
-        }
-        
-        //adjust motor values
-        
-        //write motor values
-        leftDrive1.writeValue(leftDriveValue1);
-        leftDrive2.writeValue(leftDriveValue2);
-        rightDrive1.writeValue(rightDriveValue1);
-        rightDrive2.writeValue(rightDriveValue2);
+        begin.addListener(new EventConsumer () {
+            public void eventFired() {
+                leftDrive1.writeValue(0);
+                leftDrive2.writeValue(0);
+                rightDrive1.writeValue(0);
+                rightDrive2.writeValue(0);
+            }
+        });
+
+        during.addListener(new EventConsumer() {
+            public void eventFired() {
+                //motor values
+                float leftDrive1Value = (leftDriveAxisW.readValue() + forwardDriveAxisW.readValue());
+                float leftDrive2Value = (leftDriveAxisW.readValue() + forwardDriveAxisW.readValue());
+                float rightDrive1Value = (rightDriveAxisW.readValue() + forwardDriveAxisW.readValue());
+                float rightDrive2Value = (rightDriveAxisW.readValue() + forwardDriveAxisW.readValue());
+
+                //adjust motor values
+                
+                
+                //write motor values
+                leftDrive1.writeValue(leftDrive1Value);
+                leftDrive2.writeValue(leftDrive2Value);
+                rightDrive1.writeValue(rightDrive1Value);
+                rightDrive2.writeValue(rightDrive2Value);
+            }
+        });  
     }
 
-    public static void createShifting(EventSource begin, EventSource during, BooleanOutput shiftSolenoid, BooleanInputPoll shiftHighButton, BooleanInputPoll shiftLowButton) {
+    public static void createShifting(EventSource begin, EventSource during, BooleanOutput shiftSolenoid, EventSource shiftHighButton, EventSource shiftLowButton) {
         Logger.warning("DriveCode TODO");
+        final BooleanStatus shifted = new BooleanStatus (shiftSolenoid);
+        //begin
+        shifted.setTrueWhen(begin);
+        //high
+        shifted.setTrueWhen(shiftHighButton);
+        //low
+        shifted.setFalseWhen(shiftLowButton);
     }
 }
