8bd0829b2cff0e16b6bca7c2943ed6cd5f1fd572
Gregor Peach <gregorpeach@gmail.com>
2014-02-04 16:23:15 -0800

    Fixed normalization.

--- Gemini-Apollo/src/org/team1540/geminiapollo/ControlInterface.java
+++ Gemini-Apollo/src/org/team1540/geminiapollo/ControlInterface.java
@@ -27,22 +27,23 @@ public class ControlInterface {
         return Mixing.whenBooleanBecomes(PhidgetReader.digitalInputs[1], true);
     }
 
-    public static FloatInputPoll displayPressure(final FloatInputPoll f, EventSource update) {
+    public static void displayPressure(final FloatInputPoll f, EventSource update) {
         final TuningContext tuner=new TuningContext(CluckGlobals.node,"PressureTuner");
         tuner.publishSavingEvent("Pressure");
-        final FloatInputPoll pressure = Mixing.normalizeFloat(f, tuner.getFloat("LowPressure",-3f).readValue(), tuner.getFloat("HighPressure",3f).readValue());
         update.addListener(new EventConsumer() {
             int prevValue = -1000;
             int ctr = 0;
             public void eventFired() {
-                int c = (int) (1000 * pressure.readValue());
+                float zero=tuner.getFloat("LowPressure",-3f).readValue();
+                float one=tuner.getFloat("HighPressure", 3f).readValue();
+                float range=one-zero;
+                int c = (int) (1000 * (f.readValue()-zero)/range);
                 if (c == prevValue && (ctr++ % 100 != 0)) {
                     return;
                 }
                 prevValue = c;
-                PhidgetReader.phidgetLCD[1].println("Pressure: " + prevValue / 10f + "%");
+                PhidgetReader.phidgetLCD[1].println("Pressure: " + c / 10f + "%");
             }
         });
-        return pressure;
     }
 }
