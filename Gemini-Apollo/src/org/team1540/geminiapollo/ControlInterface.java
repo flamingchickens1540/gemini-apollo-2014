faf44d08f3ab2443927ccdb8b0eee8e521e8c1ca
Gregor Peach <gregorpeach@gmail.com>
2014-02-04 17:02:50 -0800

    Tuned floats for gemini.

--- Gemini-Apollo/src/org/team1540/geminiapollo/ControlInterface.java
+++ Gemini-Apollo/src/org/team1540/geminiapollo/ControlInterface.java
@@ -30,12 +30,16 @@ public class ControlInterface {
     public static void displayPressure(final FloatInputPoll f, EventSource update) {
         final TuningContext tuner=new TuningContext(CluckGlobals.node,"PressureTuner");
         tuner.publishSavingEvent("Pressure");
+        final FloatInputPoll zeroP=tuner.getFloat("LowPressure",-3f);
+        //0.5
+        final FloatInputPoll oneP=tuner.getFloat("HighPressure", 3f);
+        //2.745
         update.addListener(new EventConsumer() {
             int prevValue = -1000;
             int ctr = 0;
             public void eventFired() {
-                float zero=tuner.getFloat("LowPressure",-3f).readValue();
-                float one=tuner.getFloat("HighPressure", 3f).readValue();
+                float zero=zeroP.readValue();
+                float one=oneP.readValue();
                 float range=one-zero;
                 int c = (int) (1000 * (f.readValue()-zero)/range);
                 if (c == prevValue && (ctr++ % 100 != 0)) {
