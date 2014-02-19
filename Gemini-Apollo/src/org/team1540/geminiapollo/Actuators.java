7b6ea4d74b35d4f85bd0cdd07edffb1a0dc4c83f
skeggsc <skeggsc@catlin.edu>
2014-02-18 16:28:01 -0800

    Added compressor override.

--- Gemini-Apollo/src/org/team1540/geminiapollo/Actuators.java
+++ Gemini-Apollo/src/org/team1540/geminiapollo/Actuators.java
@@ -1,7 +1,9 @@
 package org.team1540.geminiapollo;
 
 import ccre.chan.*;
+import ccre.cluck.CluckGlobals;
 import ccre.ctrl.Mixing;
+import ccre.event.EventConsumer;
 import ccre.event.EventSource;
 
 public class Actuators {
@@ -13,5 +15,38 @@ public class Actuators {
 
     public static void createArm(EventSource begin, EventSource during, BooleanOutput armSolenoid, BooleanInputPoll armUpDown, BooleanInputPoll canArmMove) {
         Mixing.pumpWhen(Mixing.filterEvent(canArmMove, true, during), armUpDown, armSolenoid);
+    }
+
+    public static BooleanInputPoll calcCompressorControl(EventSource counter, final BooleanInputPoll pressureSwitch) {
+        final FloatStatus compressorOverride = new FloatStatus();
+        CluckGlobals.node.publish("Compressor Override", compressorOverride);
+        CluckGlobals.node.publish("Compressor Sensor", Mixing.createDispatch(pressureSwitch, counter));
+        counter.addListener(new EventConsumer() {
+            public void eventFired() {
+                float value = compressorOverride.readValue();
+                if (value > 0) {
+                    if (value > 10) {
+                        value = 10;
+                    }
+                    value -= 0.01;
+                    if (value < 0) {
+                        value = 0;
+                    }
+                }
+                compressorOverride.writeValue(value);
+            }
+        });
+        return new BooleanInputPoll() {
+            public boolean readValue() {
+                float value = compressorOverride.readValue();
+                if (value < 0) {
+                    return true;
+                } else if (value > 0) {
+                    return false;
+                } else {
+                    return pressureSwitch.readValue();
+                }
+            }
+        };
     }
 }
