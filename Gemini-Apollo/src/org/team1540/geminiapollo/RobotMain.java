5aae65310e549079c41cdf4daadf0e205ed7ac87
skeggsc <skeggsc@catlin.edu>
2014-04-08 16:14:27 -0700

    OSU upgrades.

--- Gemini-Apollo/src/org/team1540/geminiapollo/RobotMain.java
+++ Gemini-Apollo/src/org/team1540/geminiapollo/RobotMain.java
@@ -39,6 +39,7 @@ public class RobotMain extends SimpleCore {
         //CluckGlobals.getNode().publish("Finger Override", openFingers);
         BooleanOutput armFloat = testing.testPublish("sol-float-6", makeSolenoid(6));
         BooleanOutput collectionSolenoids = Mixing.combine(openFingers, armFloat);
+        collectionSolenoids.writeValue(false);
         // ***** INPUTS *****
         final FloatInputPoll winchCurrent = makeAnalogInput(1, 8);
         final BooleanInputPoll catapultNotCocked = makeDigitalInput(2);
@@ -76,7 +77,8 @@ public class RobotMain extends SimpleCore {
         shooter.createTuner(winchCurrent, rearmEvent, catapultNotCocked);
         BooleanStatus forceRunCollectorForArmAutolower = new BooleanStatus();
         shooter.setupArmLower(ui.forceArmLower(), forceRunCollectorForArmAutolower);
-        setupCompressor(shooter.totalPowerTaken);
+        setupCompressor(Mixing.select(shooter.shouldUseCurrent, shooter.totalPowerTaken, shooter.activeAmps));
+        instinct.putCurrentActivator(shooter.shouldUseCurrent);
         // [[[[ ARM CODE ]]]]
         Actuators act = new Actuators(duringTeleop);
         act.createArm(armSolenoid, armShouldBeDown, IS_COMPETITION_ROBOT ? Mixing.alwaysFalse : shooter.rearming);
